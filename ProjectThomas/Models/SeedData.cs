f21b5adee0d91d8abccfc64d9752566bf4f2561a
Nick Rafferty <ndrafferty@hotmail.com>
2021-06-30 20:30:46 +0100

    Updated Seed Data to include images; resized images;PUBLISHED

--- ProjectThomas/Models/SeedData.cs
+++ ProjectThomas/Models/SeedData.cs
@@ -15,45 +15,105 @@ namespace ProjectThomas.Models
                     DbContextOptions<ApplicationDbContext>>()))
             {
                 // Look for any movies.
-                if (context.Book.Any())
+                if (!context.Book.Any())
                 {
-                    return;   // DB has been seeded
+                    context.Book.AddRange(
+                     new Book
+                     {
+                         Series = "Mr Men",
+                         Title = "Mr Tickle",
+                         Type = "Mr Men",
+                         Owned = true
+                     },
+
+                     new Book
+                     {
+                         Series = "Mr Men",
+                         Title = "Mr Greedy",
+                         Type = "Mr Men",
+                         Owned = true
+                     },
+
+                     new Book
+                     {
+                         Series = "Mr Men",
+                         Title = "Mr Happy",
+                         Type = "Mr Men",
+                         Owned = true
+                     },
+
+                     new Book
+                     {
+                         Series = "Mr Men",
+                         Title = "Mr Nosey",
+                         Type = "Mr Men",
+                         Owned = true
+                     }
+                 );
+                 
+                    context.SaveChanges();
+
                 }
 
-                context.Book.AddRange(
-                    new Book
-                    {
-                        Series = "Mr Men",
-                        Title = "Mr Tickle",
-                        Type = "Mr Men",
-                        Owned = true
-                    },
+                if (!context.CarouselImage.Any())
+                {
+                    context.CarouselImage.AddRange(
+                     new CarouselImage
+                     {
+                         UrlString = "/images/MrMen/MrHappy.jpg",
+                         altString = "MrHappy"
+                     },
+                     new CarouselImage
+                     {
+                         UrlString = "/images/MrMen/MrGreedy.jpg",
+                         altString = "MrGreedy"
+                     },
+                     new CarouselImage
+                     {
+                         UrlString = "/images/MrMen/MrNosey.jpg",
+                         altString = "MrNosey"
+                     },
+                     new CarouselImage
+                     {
+                         UrlString = "/images/MrMen/MrTickle.jpg",
+                         altString = "MrTickle"
+                     },
+                     new CarouselImage
+                     {
+                         UrlString = "/images/BeastQuest/Arcta.jpg",
+                         altString = "Arcta"
+                     },
+                     new CarouselImage
+                     {
+                         UrlString = "/images/BeastQuest/Epos.jpg",
+                         altString = "Epos"
+                     },
+                     new CarouselImage
+                     {
+                         UrlString = "/images/BeastQuest/Ferno.jpg",
+                         altString = "Ferno"
+                     },
+                     new CarouselImage
+                     {
+                         UrlString = "/images/BeastQuest/Nanook.jpg",
+                         altString = "Nanook"
+                     },
+                     new CarouselImage
+                     {
+                         UrlString = "/images/BeastQuest/Sepron.jpg",
+                         altString = "Sepron"
+                     },
+                     new CarouselImage
+                     {
+                         UrlString = "/images/BeastQuest/Tagus.jpg",
+                         altString = "Tagus"
+                     }
+                 );
 
-                    new Book
-                    {
-                        Series = "Mr Men",
-                        Title = "Mr Greedy",
-                        Type = "Mr Men",
-                        Owned = true
-                    },
+                    context.SaveChanges();
 
-                    new Book
-                    {
-                        Series = "Mr Men",
-                        Title = "Mr Happy",
-                        Type = "Mr Men",
-                        Owned = true
-                    },
+                }
 
-                    new Book
-                    {
-                        Series = "Mr Men",
-                        Title = "Mr Nosey",
-                        Type = "Mr Men",
-                        Owned = true
-                    }
-                );
-                context.SaveChanges();
             }
         }
     }
